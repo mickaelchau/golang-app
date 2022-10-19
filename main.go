178f80626fd17f7e7435ddd9d8ee27f845d764b4
Mickael Chau <mickaelchau@lhuillier.altostrat.com>
2022-10-19 09:46:27 +0000

    change output

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "We are really happy to handle your requests!\n")
+		return c.HTML(http.StatusOK, "Hello World!\n")
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
