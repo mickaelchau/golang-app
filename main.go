bad3ca7898c38a071e6502c4bbd41764727ada63
Mickael Chau <mickaelchau@lhuillier.altostrat.com>
2022-11-28 16:59:58 +0000

    change my output

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello world!\n")
+		return c.HTML(http.StatusOK, "Testing my new trigger!\n")
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
